e72172c4dffb575ab32654c93cdd228ed8960ef7
impallab <pallab13nayagram@gmail.com>
2024-01-19 00:10:26 +0530

    user.controller.js bug fixes ,cloudinary uploadation code comment out and only local uploadation performation code modified.

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,12 +3,28 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      cb(null, "./src/public/temp")
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      console.log(file)
+      cb(null, `${file.fieldname}-${Date.now()}.${file.mimetype.split('/')[1]}`)
     }
   })
   
   export const upload = multer({ storage: storage })
+
+  //   import multer from "multer";
+  
+  // const storage = multer.diskStorage({
+  //     destination: function (req, file, cb) {
+  //       cb(null, "./public/temp")
+  //     },
+  //     filename: function (req, file, cb) {
+  
+  //       cb(null, file.originalname)
+  //     }
+  //   })
+  
+  // export const upload = multer({ 
+  //     storage, 
+  // })
